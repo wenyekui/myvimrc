5ed581958e937615723d231971b8d2e3f838e174
wenyekui <wyk4true@gmail.com>
2013-11-16 20:21:19 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,11 @@
 #!/bin/sh
-PLUGIN_DIR=~/.vim/plugin/
+PLUGIN_DIR=~/.vim/plugin
 
 if [ ! -e $PLUGIN_DIR ];then
     mkdir -p $PLUGIN_DIR
 fi
 
-if [ ! -e $PLUGIN_DIR/mru.vim ];then
-    cp mru.vim  $PLUGIN_DIR
-fi
-
-if [ ! -e $PLUGIN_DIR/taglist.vim ];then
-    cp taglist.vim  $PLUGIN_DIR
-fi
-
-if [ ! -e $PLUGIN_DIR/cscope_map.vim ];then
-    cp cscope_map.vim  $PLUGIN_DIR
-fi
+cp -rf  plugin/*  $PLUGIN_DIR 
 
 
 
